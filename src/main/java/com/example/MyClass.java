7577ba5fe8c86ae7501a0bbbee10a0130fbaabdc
노진산(Neo) <jinsan.noh@dreamus.io>
2021-12-09 22:20:41 +0900

    step2

--- src/main/java/com/example/MyClass.java
+++ src/main/java/com/example/MyClass.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggingEventBuilder;
+
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -12,13 +15,29 @@ import java.util.logging.Logger;
  */
 public class MyClass {
 	
+	
 	public static void main(String[] args) {
+		org.slf4j.Logger mySlf4jLogger = LoggerFactory.getLogger(MyClass.class);
+		
+		mySlf4jLogger.trace("This is TRACE level log");
+		mySlf4jLogger.debug("This is DEBUG level log");
+		mySlf4jLogger.info("This is INFO level log");
+		mySlf4jLogger.warn("This is WARN level log");
+		mySlf4jLogger.error("This is ERROR level log");
+		
+		LoggingEventBuilder loggingEventBuilder = mySlf4jLogger.makeLoggingEventBuilder(org.slf4j.event.Level.WARN);
+		loggingEventBuilder.log("hello");
+		
+		System.out.println("mySlf4jLogger.getClass() = " + mySlf4jLogger.getClass());
+	}
+	
+	public static void main2(String[] args) {
 		Logger rootLogger = Logger.getLogger("");
 		// RootLogger 이름은 empty string ""
 		System.out.println("rootLogger.getName() = " + rootLogger.getName());
 		// RootLogger 의 parent 는 없다.
 		System.out.println("rootLogger.getParent() = " + rootLogger.getParent());
-		
+
 		rootLogger.setLevel(Level.ALL);
 		Handler[] rootLoggerHandlers = rootLogger.getHandlers();
 		for (Handler rootLoggerHandler : rootLoggerHandlers) {
@@ -26,21 +45,21 @@ public class MyClass {
 			System.out.println("rootLoggerHandler.getLevel() = " + rootLoggerHandler.getLevel());
 //			rootLoggerHandler.setLevel(Level.FINEST);
 		}
-		
+
 		Logger logger = Logger.getLogger("com.example.web");
 		System.out.println("logger.getParent() = " + logger.getParent());
 		System.out.println("logger.getParent().getName() = [" + logger.getParent().getName() + "]");
-		
+
 		ConsoleHandler handler = new ConsoleHandler();
 		handler.setLevel(Level.FINER);
 		logger.addHandler(handler);
-		
+
 		Handler[] handlers = logger.getHandlers();
 		for (Handler loggerHandler : handlers) {
 			System.out.println("loggerHandler = " + loggerHandler);
 			System.out.println("loggerHandler.getLevel() = " + loggerHandler.getLevel());
 		}
-		
+
 		logger.log(Level.ALL, "This is ALL level log");
 		logger.log(Level.FINEST, "This is FINEST level log");
 		logger.log(Level.FINER, "This is FINER level log");
@@ -50,7 +69,7 @@ public class MyClass {
 		logger.log(Level.WARNING, "This is WARNING level log");
 		logger.log(Level.SEVERE, "This is SEVERE level log");
 		logger.log(Level.OFF, "This is OFF level log");
-		
+
 		System.out.println("logger.getClass() = " + logger.getClass());
 	}
 	
